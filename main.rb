61a76e3772f1df23d7dfd504ef16332104695b5e
Sanjana <sanjhemdev@gmail.com>
2020-08-30 03:38:31 +0530

    Add method to check is a move is possible

--- main.rb
+++ main.rb
@@ -24,6 +24,11 @@ def clean_move_list(move_list)
   move_list
 end
 
+def possible_move?(start_location, end_location)
+  pos_moves = clean_move_list(possible_moves(start_location))
+  pos_moves.include? end_location
+end
+
 # Shows the simplest possible way to get from one square to another by outputting all squares the knight will stop on along the way
 def knight_moves
 end
@@ -35,3 +40,5 @@ end
 p possible_moves([5, 7])
 p possible_moves([5, 7]).length
 p clean_move_list(possible_moves([5, 7]))
+puts ''
+p possible_move?([5, 7], [7, 5])
