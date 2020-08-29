8a8c274594ac7760d123c06f1364b35194ff6c1a
Sanjana <sanjhemdev@gmail.com>
2020-08-30 03:22:42 +0530

    Create method to find all possible moves

--- main.rb
+++ main.rb
@@ -1,2 +1,20 @@
 # frozen-string-literal: true
 
+require_relative 'board.rb'
+
+def possible_moves(position)
+  possible_moves_array = []
+  possible_moves_array << [position[0] + 2, position[1] + 1]
+  possible_moves_array << [position[0] + 2, position[1] - 1]
+  possible_moves_array << [position[0] + 1, position[1] + 2]
+  possible_moves_array << [position[0] + 1, position[1] - 2]
+  possible_moves_array << [position[0] - 2, position[1] + 1]
+  possible_moves_array << [position[0] - 2, position[1] - 1]
+  possible_moves_array << [position[0] - 1, position[1] + 2]
+  possible_moves_array << [position[0] - 1, position[1] - 2]
+  possible_moves_array
+end
+
+# Shows the simplest possible way to get from one square to another by outputting all squares the knight will stop on along the way
+def knight_moves
+end
